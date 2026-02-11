1944f237d8d185419def796f07f89e99f13c76d9
lfcandido <luizfkz3@gmail.com>
2026-02-11 17:31:04 -0300

    feat - alteracoes nos componentes button e input

--- encurtadorFrontend/src/App.tsx
+++ encurtadorFrontend/src/App.tsx
@@ -1,12 +1,12 @@
-import Header from '@/components/header'
 
-function App() {
+import LinkCard from '@/components/linkCard'
+export default function App() {
 
   return (
     <>
-        <Header />
+      <LinkCard  url='https://www.figma.com/design/BJslDUVnpaW6TbNtmS20ng/Untitled?node-id=1-105&m=dev' cliques={876} newUrl='https://localhost:8080/rft543'/>
     </>
   )
 }
 
-export default App
+
